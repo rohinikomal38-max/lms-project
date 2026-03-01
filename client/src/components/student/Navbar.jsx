dc7c253a06a46f563599b05718e1908d1bb97506
Rohini <rohinikomal38@gmail.com>
2026-03-02 00:40:19 +0530

    feat: add testimonial section and improve homepage layout

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import { Link, useLocation } from 'react-router-dom'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+import { AppContext } from '../../context/AppContext'
 
 const Navbar = () => {
+
+  const {navigate, isEducator} = useContext(AppContext)
 
 const location = useLocation();
 const isCourseListPage = location.pathname.includes('/course-list');
@@ -16,7 +19,7 @@ const {user} = useUser()
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14
     lg:px-36 border-b border-gray-500 py-4  ${isCourseListPage ? 'bg-white' : 
     'bg-cyan-100/70'}`}>
-      <img src={assets.logo} alt="Logo" className='w-28 lg:w-32
+      <img onClick={()=> navigate('/')} src={assets.logo} alt="Logo" className='w-28 lg:w-32
       cursor-pointer' />
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
         
@@ -24,7 +27,8 @@ const {user} = useUser()
         <div className='flex items-center gap-5'>
           { user && 
           <>
-            <button> Become Educator</button>
+            <button onClick={()=> {navigate('/educator')}}>{isEducator ?
+             'Educator Dashboard' : 'Become Educator'}</button>
             | <Link  to='/my-enrollments'>My Enrollments</Link>
           </>
           }               
@@ -40,7 +44,8 @@ const {user} = useUser()
         <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
           { user && 
           <>
-            <button> Become Educator</button>
+            <button onClick={()=> {navigate('/educator')}}>{isEducator ?
+             'Educator Dashboard' : 'Become Educator'}</button>
             | <Link  to='/my-enrollments'>My Enrollments</Link>
           </>
           }  
