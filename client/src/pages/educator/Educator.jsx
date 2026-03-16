18ce2cb7292fdd2b0acf942feb6d926bc5c29de4
Rohini <rohinikomal38@gmail.com>
2026-03-16 22:03:13 +0530

    Completed frontend: dashboard, my courses, student enrolled, and add course page

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import NavBar from '../../components/educator/NavBar'
+import Sidebar from '../../components/educator/Sidebar'
+import Footer from '../../components/educator/Footer'
 
 const Educator = () => {
   return (
-    <div>
-      <h1>Educator page</h1>
-      <div>
+    <div className='text-default min-h-screen bg-white'>
+      <NavBar />
+      <div className='flex'>
+        <Sidebar />
+        <div className='flex-1'>
         {<Outlet/>}
-
+     </div>
       </div>
+      <Footer/>
     </div>
   )
 }
