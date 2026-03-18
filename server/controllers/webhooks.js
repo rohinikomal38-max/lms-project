82aaad80ad5ea099dcc29ba158197b0e42c909de
Rohini <rohinikomal38@gmail.com>
2026-03-18 18:49:02 +0530

    fix(webhook): correct Clerk webhook user data handling

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -19,7 +19,7 @@ export const clerkWebhooks = async (req, res)=>{
         switch (type) {
             case 'user.created': {
                 const userData ={
-                    _id: data._id,
+                    _id: data.id,
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
