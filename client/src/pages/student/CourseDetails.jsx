b358d75b300ecbca3aa162c40780a7de4733446b
rohinikomal38-max <rohinikomal38@gmail.com>
2026-08-07 18:58:52 +0530

    Update course author name in CourseDetails component

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -87,7 +87,7 @@ useEffect(() => {
               </div>
 
               <p className='text-sm'>Course by <span className='text-blue-600
-              underline'>GreatStack</span></p>
+              underline'>RohiniKomal</span></p>
 
               <div className='pt-8 text-gray-800'>
                   <h2 className='text-xl font-semibold'>Course Structure</h2>
