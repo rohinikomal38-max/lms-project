e9cf281c6914047905233ad69ccabef29f855447
rohinikomal38-max <rohinikomal38@gmail.com>
2026-08-07 19:22:07 +0530

    Add review route with user protection

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { getAllCourse, getCourseId, addReview } from '../controllers/courseController.js';
+import { protectUser } from '../middlewares/authMiddleware.js';
 
 const courseRouter = express.Router()
 
 courseRouter.get('/all', getAllCourse)
 courseRouter.get('/:id', getCourseId)
+
+// Add Review
+courseRouter.post('/review', protectUser, addReview);
 
 export default courseRouter;
