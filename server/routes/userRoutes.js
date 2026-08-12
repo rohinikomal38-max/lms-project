646f34d906b2259b994b6396b8200a944377e3ef
rohinikomal38-max <rohinikomal38@gmail.com>
2026-08-12 13:54:57 +0530

    Fix import path for authMiddleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses, enrollCourse } from '../controllers/userController.js'
-import { protectUser } from '../middleware/authMiddleware.js'
+import { protectUser } from '../middlewares/authMiddleware.js'
 
 const userRouter = express.Router()
 
