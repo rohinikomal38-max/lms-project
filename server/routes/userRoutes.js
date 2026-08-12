64e8387d274e32e9ffa39e70c04317a6a4ad4eb9
rohinikomal38-max <rohinikomal38@gmail.com>
2026-08-12 21:46:23 +0530

    Add user protection middleware to purchase route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,7 +6,7 @@ const userRouter = express.Router()
 
 userRouter.get('/data', getUserData)
 userRouter.get('/enrolled-courses', userEnrolledCourses)
-userRouter.post('/purchase', purchaseCourse)
+userRouter.post('/purchase', protectUser, purchaseCourse)
 
 userRouter.post('/update-course-progress', updateUserCourseProgress)
 userRouter.post('/get-course-progress', getUserCourseProgress)
